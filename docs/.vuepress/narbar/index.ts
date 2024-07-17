be0c2a6ef68d7d193fd6c41d9611dcb37f0d20f3
fanmei <merrillfan@aliyun.com>
2024-07-17 16:41:12 +0800

    init

---
+++ docs/.vuepress/narbar/index.ts
@@ -0,0 +1,18 @@
+export const navbar = [
+    {
+        text: '主页',
+        link: '/',
+    },
+    {
+        text: '语言',
+        children:
+            [
+                {text: 'C', link: 'https://code.iglooblog.top/c/1.html'},
+                {text: 'Js', link: '/js/'}
+            ]
+    },
+    {
+        text: '关于',
+        link: '/about',
+    }
+]

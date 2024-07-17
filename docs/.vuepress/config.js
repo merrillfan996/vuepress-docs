09dd789335e4a31026b48bf83c72425c820df345
fanmei <merrillfan@aliyun.com>
2024-07-17 17:16:24 +0800

    init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,14 @@
 import {defaultTheme} from '@vuepress/theme-default'
 import {defineUserConfig} from 'vuepress/cli'
 import {viteBundler} from '@vuepress/bundler-vite'
-import  vuepressPluginAnchorRight from "vuepress-plugin-anchor-right"
 import {navbar} from "./narbar/index.js";
 import {sidebar} from "./sidebar/index.js";
 
 export default defineUserConfig({
-    title: '星空文档', // 显示在左上角的网页名称以及首页在浏览器标签显示的title名称
+    title: '星空文档',
+    base: "/",
     description: 'merrill-fan', // meta 中的描述文字，用于SEO
-    head: [
-        ['link', {rel: 'icon', href: '/star.png'}],
-    ],
+    head: [['link', {rel: 'icon', href: '/star.png'}],],
     theme: defaultTheme({
         logo: "star.png",
         navbar: navbar,
